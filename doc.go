078c61a9786bb742ee9db24b2603aa0303c5715a
Hiro <goferhiro@gmail.com>
2022-11-12 08:46:10 +0530

    :memo: docs
    Signed-off-by: Hiro <goferHiro@gmail.com>

---
+++ doc.go
@@ -0,0 +1,34 @@
+/*
+
+# Project Structure Reference
+
+https://github.com/golang-standards/project-layout
+
+# Functional Options Pattern
+
+https://golang.cafe/blog/golang-functional-options-pattern.html
+
+# Dependency Management
+
+[https://www.youtube.com/watch?v=LDGKQY8WJEM] (KT on advanced dependency management)
+[https://www.youtube.com/watch?v=LDGKQY8WJEM]( A session by fx team at UBER)
+
+
+
+# Code Of Conduct
+
+1. Golang best practices
+2. No memory leaks
+3. Do memory profiling using pprof & gcvis
+4. Document your higher level abstractions in doc.go
+5. Document your fc, methods, and other datatypes using doc comments
+6. Consider GC overhead before using *p and interfaces
+7. Use race detectors (-race) to detect race conditions
+
+# Documenting your code
+
+1. https://go.dev/doc/comment
+
+*/
+
+package user_svc
--- test/doc.go
+++
@@ -1,9 +0,0 @@
-/*
-1. There is a test middleware that will register routes dynamically to test db.
-2. the sample requests are cached too.
-3. during initialization tests are conducted based on the smaple reqs and response from db.
-4. Use this for testing
-
-*/
-
-package main
