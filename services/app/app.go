fa0d795c9d70f35e3a264cc963bbab5b1c067ea1
Hiro <goferHiro@gmail.com>
2022-11-14 12:32:13 +0530

    :sparkles: app service
    Signed-off-by: Hiro <goferHiro@gmail.com>

---
+++ services/app/app.go
@@ -0,0 +1,7 @@
+package app
+
+import "github.com/BzingaApp/user-svc/internal/genesis"
+
+type service struct {
+	*genesis.Service
+}
--- services/app/fx.go
+++ services/app/fx.go
@@ -1,1 +1,36 @@
 package app
+
+import (
+	"github.com/BzingaApp/user-svc/internal/genesis"
+	"go.uber.org/fx"
+)
+
+var Module = fx.Options(
+	fx.Provide(
+		newServices,
+	),
+)
+
+type In struct {
+	fx.In
+	*genesis.Service
+}
+
+type Out struct {
+	fx.Out
+
+	AppServices Services
+}
+
+func newServices(i In) (o Out) {
+	o = Out{
+		AppServices: newApp(i.Service),
+	}
+	return
+}
+
+func newApp(genesis *genesis.Service) Services {
+	return &service{
+		genesis,
+	}
+}
